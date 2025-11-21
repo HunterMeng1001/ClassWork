b2308c8af76886b258e7ba0cdf6e93684d69f250
HunterMeng1001 <166929222+HunterMeng1001@users.noreply.github.com>
2025-11-21 15:41:09 +0800

    feat: 第一个页面实现

--- app/src/main/java/com/git/classwork/android/MainActivity.kt
+++ app/src/main/java/com/git/classwork/android/MainActivity.kt
@@ -1,47 +1,13 @@
 package com.git.classwork.android
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.git.classwork.android.ui.theme.ClassWorkTheme
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ClassWorkTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ClassWorkTheme {
-        Greeting("Android")
-    }
-}
